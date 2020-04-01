e8de36364085ceae5050ce60d3dc8da98241e15e
Dinika <dinika.jia2@gmail.com>
2020-04-01 13:06:36 +0100

    Revert "mod ./install.sh"
    This reverts commit 1c21f730a84972f7b8fe454345294bb51711895b.

--- install.sh
+++ install.sh
@@ -2,54 +2,50 @@
 cd ${0%/*} || exit 1 # Run from this directory
 
 echo "Installing NUFEB.."
-rootDir=$PWD
+currentDir=$PWD
 
 #### Copy package and lib files to LAMMPS directory #####
 echo "Copying packages to LAMMPS.."
-cp -rf $rootDir/src/* $rootDir/lammps/src/
-cp -rf $rootDir/lib/* $rootDir/lammps/lib/
+cp -rf $currentDir/src/* $currentDir/lammps/src/
+cp -rf $currentDir/lib/* $currentDir/lammps/lib/
 
 echo "Configuring Makefile.lammps.."
 
-cd $rootDir/lammps/lib/nufeb
-cp Makefile.lammps_essential Makefile.lammps
-
-declare -i vtk_hdf=0
+cd $currentDir/lammps/lib/nufeb
 
 for var in "$@"
 do 
     if [ $var == "--enable-vtk" ] ; then
        cp Makefile.lammps_vtk8.0 Makefile.lammps
-       cp ../vtk/Makefile.lammps_vtk8.0 ../vtk/Makefile.lammps
-       vtk_hdf=$((vtk_hdf+1))
+       cd ../vtk
+       cp Makefile.lammps_vtk8.0 Makefile.lammps
     elif [ $var == "--enable-hdf5" ]; then
        cp Makefile.lammps_hdf5 Makefile.lammps
-       vtk_hdf=$((vtk_hdf+1))
-    elif [ $var == "--static" ]; then continue
-    elif [ $var == "--shared" ]; then continue
+    elif [ $var == "--enable-vtk-hdf5" ]; then
+       cp Makefile.lammps_hdf5_vtk8.0 Makefile.lammps
+       cd ../vtk
+       cp Makefile.lammps_vtk8.0 Makefile.lammps
     elif [ $var == "--serial" ]; then continue
     else
-       echo "Unknown parameter"
-       exit 1
+       if [ $var != "--serial" ]; then
+          echo "Unknown parameter"
+          exit 1
+       fi
     fi
 done
 
-if [ $vtk_hdf = 2 ]; then
-echo $PWD
-    cp Makefile.lammps_hdf5_vtk8.0 Makefile.lammps
-fi
 
 #### Build LAMMPS with NUFEB and VTK packages#####
 echo "Installing required packages.."
 
-cd $rootDir/lammps/src
+cd $currentDir/lammps/src
 make yes-user-nufeb
 make yes-granular
 make yes-user-psoriasis
 
 for var in "$@"
 do 
-    if [ $var == "--enable-vtk" ]; then
+    if [ $var == "--enable-vtk" ] || [ $var == "--enable-vtk-hdf5" ]; then
 	make yes-user-vtk
     fi
 done
@@ -62,37 +58,20 @@ do
         make
         cd ..
         make -j4 serial
-        exit 1
-   fi
+<<<<<<< HEAD
+    else 
+        make -j4 mpi
+    fi
 done
 
-for var in "$@"
-do 
-    if [ $var == "--static" ]; then
-        make -j4 mpi mode=lib
-        exit 1
-   fi
-done
-
-for var in "$@"
-do 
-    if [ $var == "--shared" ]; then
-        make -j4 mpi mode=shlib
+=======
         exit 1
    fi
 done
 
 make -j4 mpi
-
-path=$rootDir/lammps/src
-if grep -q $path ~/.bashrc; then
-  echo -n
-else
-  echo "Writing path to .bashrc"
-  echo "export PATH=\$PATH:$path" >> ~/.bashrc
-fi
-
 exit 1
 
-
-
+>>>>>>> master
+#echo "Writing path to .bashrc"
+#echo "export PATH=\$PATH:$currentDir/lammps/src/" >> ~/.bashrc
